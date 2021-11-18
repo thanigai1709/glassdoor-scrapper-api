5eb3f327308def7a048f9252448b558167ad1b17
Thanigai <thanigai@excrin.com>
2021-11-18 21:15:59 +0530

    scrapper function

---
+++ server.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const targetUrl =
+  'https://www.glassdoor.co.in/Reviews/Sedin-Technologies-Reviews-E382372.htm'
+
+const express = require('express')
+const app = express()
+
+app.get('/', async function (req, res) {
+  axios
+    .get(targetUrl)
+    .then((page) => {
+      const $ = cheerio.load(page.data);
+      const companyDetails = $('#EiReviewsContainer h1.eiReviews__EIReviewsPageStyles__newPageHeader').html();
+      const overalReview = $('#EiReviewsContainer h2[data-test="overallReviewCount"]').html();
+      const ratingHTML = $(
+        '#EmpStats .v2__EIReviewsRatingsStylesV2__ratingInfo',
+      ).html()
+      const rating = $(
+        '#EmpStats .v2__EIReviewsRatingsStylesV2__ratingNum',
+      ).html()
+      res.send({companyDetails, rating, ratingHTML, overalReview })
+    })
+    .catch((err) => {
+      res.status(502).send(err)
+    })
+})
+
+app.listen(3000, () => {
+  console.log('Scrapper runs on PORT 3000')
+})
